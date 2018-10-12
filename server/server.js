be3fc0ad56b30904947de05cc724a07655eb132e
EddieLo <eddielo106@gmail.com>
2018-10-12 16:22:51 -0700

    setup proxy

--- server/server.js
+++ server/server.js
@@ -7,16 +7,20 @@ const path = require('path');
 const compression = require('compression');
 
 const app = express();
-const port = process.env.PORT || 9999;
+const port = process.env.PORT || 3000;
 
 app.use(compression());
-// app.use(morgan('dev'));
+app.use(morgan('tiny'));
 
 console.log(path.join(__dirname, 'public'))
 
-app.use('/:companyAbbr', express.static(path.join(__dirname, '../public')));
+app.use('/:abbrOrId', express.static(path.join(__dirname, '../public')));
 
 app.get('/api/people-also-bought/:abbrOrId', proxy('http://ec2-54-219-140-91.us-west-1.compute.amazonaws.com'));
+
+app.get('/api/stockPricePoints/:abbrOrId', proxy('http://ec2-52-53-158-219.us-west-1.compute.amazonaws.com'));
+
+app.get('/api/graph/:abbrOrId', proxy('http://ec2-18-218-247-23.us-east-2.compute.amazonaws.com'));
 
 app.listen(port, () => {
   console.log(`server running at: http://localhost:${port}`);
